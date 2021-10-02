0a70464ae8b4114cf81263a111efb7cb88e6a795
Leanne <76232496+le-anne@users.noreply.github.com>
2021-10-01 19:30:08 -0700

    edited quotes

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const quotes = [
-  "Help me, Obi-Wan Kenobi. You're my only hope.",
+  "Help me, Obi-Wan Kenobi. You\'re my only hope.",
   "I find your lack of faith disturbing.",
-  "It's the ship that made the Kessel run in less than twelve parsecs. I've outrun Imperial starships. Not the local bulk cruisers, mind you. I'm talking about the big Corellian ships, now. She's fast enough for you, old man.",
+  "It\'s the ship that made the Kessel run in less than twelve parsecs. I\'ve outrun Imperial starships. Not the local bulk cruisers, mind you. I\'m talking about the big Corellian ships, now. She\'s fast enough for you, old man.",
   "The Force will be with you. Always.",
   "Why, you stuck-up, half-witted, scruffy-looking nerf herder!",
   "Never tell me the odds!",
   "Do. Or do not. There is no try.",
   "No. I am your father.",
   "When gone am I, the last of the Jedi will you be. The Force runs strong in your family. Pass on what you have learned.",
-  "I'll never turn to the dark side. You’ve failed, your highness. I am a Jedi, like my father before me.",
-  "You can't stop the change, any more than you can stop the suns from setting.",
+  "I\'ll never turn to the dark side. You\’ve failed, your highness. I am a Jedi, like my father before me.",
+  "You can\'t stop the change, any more than you can stop the suns from setting.",
   "Fear is the path to the dark side. Fear leads to anger; anger leads to hate; hate leads to suffering. I sense much fear in you.",
   "Well, if droids could think, there'd be none of us here, would there?",
   "We must keep our faith in the Republic. The day we stop believing democracy can work is the day we lose it.",
-  "I'm just a simple man trying to make my way in the universe.",
-  "Chewie, we're home."
+  "I\'m just a simple man trying to make my way in the universe.",
+  "Chewie, we\'re home."
 ];
 
 const quote = document.getElementById("quote");
